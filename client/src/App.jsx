fc056123df20b6544d35465d33a14f0c629a1b15
Ro Min Swe <rominswe@gmail.com>
2025-10-21 08:46:55 +0800

    Project FrameWork Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="h-screen flex flex-col justify-center items-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Tailwind is Working 🎉</h1>
+      <p className="text-gray-700 mt-3">Your React + Tailwind setup is ready!</p>
+    </div>
+  );
+}
